848a181bece34a4144209bcbca4e519087d3a6fe
kaiwichkornEiEi <koonguy242629@gmail.com>
2024-07-23 11:55:48 +0700

    week4_

---
+++ kaiwichkorn-remix-app/app/routes/data.tsx
@@ -0,0 +1,44 @@
+export const sculptureList=[
+{
+name: '1.Motorcycle bmw ไฟฟ้า',
+artist: 'kaiwichkorn',
+description:'An advanced electric motorcycle with an exciting price of 859,000 baht, 1 charge, 130 kilometers',
+url:'https://f.ptcdn.info/159/079/000/rm54xou5l90DU1llggq-o.jpg',
+alt:'An innovative electric motorcycle  1 charge, 130 kilometers',
+},
+{
+name: '2.สวนสนุก dreamworld',
+artist: 'kaiwichkorn',
+description:'Impressed by the International Parade Led by Dreamy, along with cartoon stars and actors in colorful costumes.',
+url:'https://th.bing.com/th/id/OIP.7obxM7kRMQmQDnRsehQR5AAAAA?rs=1&pid=ImgDetMain',
+ alt:' Along with cartoon stars and actors in colorful costumes',
+},
+{
+name: '3.เสือดาว',
+artist: 'kaiwichkorn',
+description:'The leopard has relatively short legs, and has a long body and a large skull.',
+url:'https://hilight.kapook.com/img_cms2/user/rungtip/2019/_599738204.jpg',
+alt:'The leopard has relatively short legs, and has a long body and a large skull.',
+},
+{
+name: '4.ซาล่าห์',
+artist: 'kaiwichkorn',
+description:'ซาล่าห์เซ็นสัญญากับสโมสรลิเวอร์พูล ค่าตัวกว่า 50 ล้านปอนด์ ส่งผลให้เขากลายเป็นนักฟุตบอลสถิติค่าตัวสโมสรเมอร์ซีย์ไซด์ทันที',
+url:'https://medias.thansettakij.com/media/images/2020/11/13/1605283607_3.jpeg',
+alt:'ซาล่าห์เซ็นสัญญากับสโมสรลิเวอร์พูล ค่าตัวกว่า 50 ล้านปอนด์ ',
+},
+{
+name: '5.รถ ปอร์เช่',
+artist: 'kaiwichkorn',
+description:' บริษัทผู้ผลิตและจำหน่ายรถสปอร์ตและรถสมรรถนะสูงจากเยอรมนี ปัจจุบันทำตลาดผลิตภัณฑ์หลายรุ่น',
+url:'https://th.bing.com/th/id/R.0323c1863e72aa3971bae209ef81ba25?rik=T38FasP97C2tnA&pid=ImgRaw&r=0',
+alt:' บริษัทผู้ผลิตและจำหน่ายรถสปอร์ตและรถสมรรถนะสูงจากเยอรมนี ปัจจุบันทำตลาดผลิตภัณฑ์หลายรุ่น',
+},
+{
+name: '6.srarun',
+artist: 'kaiwichkorn',
+description:'การแข่งขันในรายการ The Rapper ของ SARAN ได้เริ่มตอนที่เขามีอายุ 13 ปี เมื่อเขาได้ออดิชั่นด้วยการแร็ปเล่ามุมมองความรักของเด็กวัยรุ่นในเพลง',
+url:'https://www.brighttv.co.th/wp-content/uploads/2022/06/1655359607276.jpg',
+alt:'การแข่งขันในรายการ The Rapper ของ SARAN ได้เริ่มตอนที่เขามีอายุ 13 ปี เมื่อเขาได้ออดิชั่นด้วยการแร็ปเล่ามุมมองความรักของเด็กวัยรุ่นในเพลง',
+}
+];
